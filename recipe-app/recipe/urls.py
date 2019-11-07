f090f1ea5ed47e7f6f7818b1059e3aceaeff4abf
lauren robinson <laurenrobinson@laurens-MacBook-Air.local>
2019-11-07 12:49:48 -0500

    full crud

--- recipe-app/recipe/urls.py
+++ recipe-app/recipe/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('comments/', views.comment_list, name='comment_list'),
     path('recipes/<int:pk>', views.recipe_detail, name='recipe_detail'),
     path('recipes/new', views.recipe_create, name='recipe_create'),
-    path('comments/new', views.comment_create, name='comment_create')
+    path('comments/new', views.comment_create, name='comment_create'),
+    path('recipes/<int:pk>/edit', views.recipe_edit, name='recipe_edit'),
+    path('recipes/<int:pk>/delete', views.recipe_delete, name='recipe_delete'),
+    path('comments/<int:pk>/delete', views.comment_delete, name='comment_delete')
 ]
--- recipe-app/recipe/views.py
+++ recipe-app/recipe/views.py
@@ -9,24 +9,43 @@ def comment_list(request):
     comments = Comments.objects.all()
     return render(request, 'recipe/comment_list.html', {'comments': comments})
 def recipe_detail(request, pk):
-    recipes = Recipe.objects.get(id=pk)
+    recipes = Recipe.objects.get(pk=pk)
     return render(request, 'recipe/recipe_detail.html', {'recipes': recipes})
 def recipe_create(request):
     if request.method == 'POST':
         form = RecipeForm(request.POST)
         if form.is_valid():
-            artist = form.save()
-            return redirect('recipe_detail', pk=recipe.pk)
+            recipes = form.save()
+            return redirect('recipe_detail', pk=recipes.pk)
     else:
         form = RecipeForm()
     return render(request, 'recipe/recipe_form.html', {'form': form})
+
 def comment_create(request):
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
-            comment = form.save()
-            return redirect('recipe_detail', pk=recipe.pk)
+            comments = form.save()
+            return redirect('recipe_detail', pk=recipes.pk)
     else:
         form = CommentForm()
     return render(request, 'recipe/comment_form.html', {'form': form})
+
+def recipe_edit(request, pk):
+    recipes = Recipe.objects.get(pk=pk)
+    if request.method == "POST":
+        form = RecipeForm(request.POST, instance=recipes)
+        if form.is_valid():
+            recipes = form.save()
+           
+            return redirect('recipe_detail', pk=recipes.pk)
+    else:
+        form = RecipeForm(instance=recipes)
+    return render(request, 'recipe/recipe_form.html', {'form': form})
+def recipe_delete(request, pk):
+    Recipe.objects.get(id=pk).delete()
+    return redirect('recipe_list')
+def comment_delete(request,pk):
+    Comments.objects.get(id=pk).delete()
+    return redirect('recipe_detail')
 # Create your views here.
